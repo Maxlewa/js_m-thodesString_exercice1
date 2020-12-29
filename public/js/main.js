c4f498dc1ec54530bfd177f0d2bffa3b9151c3bc
Maxence <lemairemaxence@hotmail.com>
2020-12-29 14:50:20 +0100

    correction

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,51 @@
 // EXO 1
 
 let phrase = 'Bonjour tout le monde      '
-console.log(`${phrase.length}`);
+console.log(phrase.length);
 
-let phraseNoFinalSpace = phrase.trim()
-// console.log(phraseNoFinalSpace);
-console.log(`${phraseNoFinalSpace.length}`);
+// trim marche aussi
+phrase = phrase.trimEnd()
+console.log(phrase.length);
 
-console.log(`${phraseNoFinalSpace.slice(-1)}`);
+// slice cumulatif
+// console.log(phrase.slice(-1));
 
-// console.log(`${phraseNoFinalSpace.replace('Bonjour ', '')}`);
+// cibler l'élément
+console.log(phrase.charAt(phrase.length -1));
+
+// console.log(`${phrase.replace('Bonjour ', '')}`);
 
 // let phraseNoBonjour = 'tout le monde'
 // let bonjour = 'Bonjour '
 // let phraseComplete = bonjour.concat(phraseNoBonjour);
 // console.log(phraseComplete);
 
-console.log(`${phraseNoFinalSpace.substr(0, 7)} ${phraseNoFinalSpace.slice(8)}`);
+// console.log(`${phrase.substr(0, 7)} ${phrase.slice(8)}`);
 
-console.log(`${phraseNoFinalSpace.replace(' tout le monde', '')}`);
+phrase = phrase.replace('Bonjour ', "")
+console.log(phrase);
+
+// phrase = phrase.replace('tout le monde', '')
+// console.log(phrase);
+
+// console.log(`${phrase.replace(' tout le monde', '')}`);
+
+let bonjour = "Bonjour "
+phrase = bonjour.concat("", phrase)
+console.log(phrase);
+
+phrase = phrase.substring(0,8)
+console.log(phrase);
 
 // EXO 2
 
 let phrase2 = "Ceci est une variable de type number !"
-let phrase2new = phrase2.replace("number", "string");
-console.log(phrase2new);
+phrase2 = phrase2.replace("number", "string");
+console.log(phrase2);
 
 // EXO 3
 
 let phrase3 = "Ceci est une variable de type NuMbEr ! Les numbers c'est trop bien Number numBer numbeR"
-// let phrase3Lowercase = phrase3.toLowerCase();
-let phrase3Lowercase = phrase3.replace("NuMbEr","number").replace("Number","number").replace("numBer","number").replace("numbeR","number")
-console.log(phrase3Lowercase);
+// phrase3 = phrase3.replace("NuMbEr","number").replace("Number","number").replace("numBer","number").replace("numbeR","number")
+phrase3 = phrase3.replace(/number/ig, "number");
+console.log(phrase3);
